2adc58fb6cea3af25e3db2057e00aeaac8484448
fastmans <43810849+fastmans@users.noreply.github.com>
2018-10-14 04:22:15 +0200

    update sound

--- p/h/sketch.js
+++ p/h/sketch.js
@@ -4,13 +4,13 @@ var sound = new Howl({
 
 function setup() {
   var button = createButton('Play');
-  button.mousePressed(console.log("played"));
+  button.mousePressed(myFunction);
 }
 
 function draw() {
   background(0,0,155);
 }
 
-// function myFunction() {
-//   sound.play();
-// }
+function myFunction() {
+  sound.play();
+}
