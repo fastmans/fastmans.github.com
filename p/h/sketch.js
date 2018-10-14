87ba925fcea1f60f79787e54b221c7598e0e4551
fastmans <43810849+fastmans@users.noreply.github.com>
2018-10-14 04:39:49 +0200

    p5 Page

--- p/h/sketch.js
+++ p/h/sketch.js
@@ -1,16 +1,22 @@
 function setup() {
-  var button = createButton('Play');
-  button.mousePressed(myFunction);
+  createCanvas(800,600);
 }
 
 function draw() {
   background(0,0,155);
+  fill(0,155,0);
+  rect(200,200,400,200);
+  fill(0);
+  textSize(128);
+  text('Play', 280, 340);
 }
 
-function myFunction() {
-  var sound = new Howl({
-    src: ['audio/jef.mp3']
-  });
+function mousePressed(){
+  if(mouseX>200 && mouseX<600 && mouseY>200 && mouseY<400){
+    var sound = new Howl({
+      src: ['audio/jef.mp3']
+    });
 
-  sound.play();
+    sound.play();
+  }
 }
