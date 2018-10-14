789b70e3c5ea5848986f53ee83ffbfdf46ca29ab
fastmans <43810849+fastmans@users.noreply.github.com>
2018-10-14 18:38:13 +0200

    web dizajn

---
+++ p/web_dizajn/script.js
@@ -0,0 +1,174 @@
+function hideQuestion1() {
+  if(document.getElementById("hiddenQuestion1").style.display == "block"){
+    document.getElementById("hiddenQuestion1").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion1").style.display = "block";
+  }
+
+}
+function hideQuestion2() {
+  if(document.getElementById("hiddenQuestion2").style.display == "block"){
+    document.getElementById("hiddenQuestion2").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion2").style.display = "block";
+  }
+
+}
+function hideQuestion3() {
+  if(document.getElementById("hiddenQuestion3").style.display == "block"){
+    document.getElementById("hiddenQuestion3").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion3").style.display = "block";
+  }
+
+}
+function hideQuestion4() {
+  if(document.getElementById("hiddenQuestion4").style.display == "block"){
+    document.getElementById("hiddenQuestion4").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion4").style.display = "block";
+  }
+
+}
+function hideQuestion5() {
+  if(document.getElementById("hiddenQuestion5").style.display == "block"){
+    document.getElementById("hiddenQuestion5").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion5").style.display = "block";
+  }
+
+}
+function hideQuestion6() {
+  if(document.getElementById("hiddenQuestion6").style.display == "block"){
+    document.getElementById("hiddenQuestion6").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion6").style.display = "block";
+  }
+
+}
+function hideQuestion7() {
+  if(document.getElementById("hiddenQuestion7").style.display == "block"){
+    document.getElementById("hiddenQuestion7").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion7").style.display = "block";
+  }
+
+}
+function hideQuestion8() {
+  if(document.getElementById("hiddenQuestion8").style.display == "block"){
+    document.getElementById("hiddenQuestion8").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion8").style.display = "block";
+  }
+
+}
+function hideQuestion9() {
+  if(document.getElementById("hiddenQuestion9").style.display == "block"){
+    document.getElementById("hiddenQuestion9").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion9").style.display = "block";
+  }
+
+}
+function hideQuestion10() {
+  if(document.getElementById("hiddenQuestion10").style.display == "block"){
+    document.getElementById("hiddenQuestion10").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion10").style.display = "block";
+  }
+
+}
+function hideQuestion11() {
+  if(document.getElementById("hiddenQuestion11").style.display == "block"){
+    document.getElementById("hiddenQuestion11").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion11").style.display = "block";
+  }
+
+}
+function hideQuestion12() {
+  if(document.getElementById("hiddenQuestion12").style.display == "block"){
+    document.getElementById("hiddenQuestion12").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion12").style.display = "block";
+  }
+
+}
+function hideQuestion13() {
+  if(document.getElementById("hiddenQuestion13").style.display == "block"){
+    document.getElementById("hiddenQuestion13").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion13").style.display = "block";
+  }
+
+}
+function hideQuestion14() {
+  if(document.getElementById("hiddenQuestion14").style.display == "block"){
+    document.getElementById("hiddenQuestion14").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion14").style.display = "block";
+  }
+
+}
+function hideQuestion15() {
+  if(document.getElementById("hiddenQuestion15").style.display == "block"){
+    document.getElementById("hiddenQuestion15").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion15").style.display = "block";
+  }
+
+}
+function hideQuestion16() {
+  if(document.getElementById("hiddenQuestion16").style.display == "block"){
+    document.getElementById("hiddenQuestion16").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion16").style.display = "block";
+  }
+
+}
+function hideQuestion17() {
+  if(document.getElementById("hiddenQuestion17").style.display == "block"){
+    document.getElementById("hiddenQuestion17").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion17").style.display = "block";
+  }
+
+}
+function hideQuestion18() {
+  if(document.getElementById("hiddenQuestion18").style.display == "block"){
+    document.getElementById("hiddenQuestion18").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion18").style.display = "block";
+  }
+
+}
+function hideQuestion19() {
+  if(document.getElementById("hiddenQuestion19").style.display == "block"){
+    document.getElementById("hiddenQuestion19").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion19").style.display = "block";
+  }
+
+}
+function hideQuestion20() {
+  if(document.getElementById("hiddenQuestion20").style.display == "block"){
+    document.getElementById("hiddenQuestion20").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion20").style.display = "block";
+  }
+
+}
+function hideQuestion21() {
+  if(document.getElementById("hiddenQuestion21").style.display == "block"){
+    document.getElementById("hiddenQuestion21").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion21").style.display = "block";
+  }
+
+}
+
+document.addEventListener('mousedown', function (event) {
+  if (event.detail > 1) {
+    event.preventDefault(); // of course, you still do not know what you prevent here... You could also check event.ctrlKey/event.shiftKey/event.altKey to not prevent something useful.
+  }
+}, false);
