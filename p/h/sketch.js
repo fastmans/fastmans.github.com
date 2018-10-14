1acfdfb051fa1e40b7fec2129700be25c0450f90
fastmans <43810849+fastmans@users.noreply.github.com>
2018-10-14 03:56:15 +0200

    Update p/h/sketch.js

--- p/h/sketch.js
+++ p/h/sketch.js
@@ -1,11 +1,15 @@
 let mySound;
 
-function preload(){
-  mySound = loadSound('jef.mp3');
+function preload() {
+  soundFormats('mp3', 'ogg');
+  mySound = loadSound('audio/jef.mp3');
 }
 
 function setup() {
   createCanvas(800,600);
+
+  mySound.setVolume(0.1);
+  mySound.play();
 
   var button = createButton('Play');
   button.mousePressed(myFunction());
@@ -15,7 +19,7 @@ function draw() {
   background(0,0,155);
 }
 
-function myFunction() {
-  mySound.setVolume(0.1);
-  mySound.play(mySound);
-}
+// function myFunction() {
+//   mySound.setVolume(0.1);
+//   mySound.play(mySound);
+// }
