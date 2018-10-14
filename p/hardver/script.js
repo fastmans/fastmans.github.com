f883503737c39516f601e4fe4c5cd9bf0f54da30
fastmans <43810849+fastmans@users.noreply.github.com>
2018-10-14 18:00:46 +0200

    Hardver

---
+++ p/hardver/script.js
@@ -0,0 +1,102 @@
+function hideQuestion1() {
+  if(document.getElementById("hiddenQuestion1").style.display == "block"){
+    document.getElementById("hiddenQuestion1").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion1").style.display = "block";
+  }
+
+}
+function hideQuestion2() {
+  if(document.getElementById("hiddenQuestion2").style.display == "block"){
+    document.getElementById("hiddenQuestion2").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion2").style.display = "block";
+  }
+
+}
+function hideQuestion3() {
+  if(document.getElementById("hiddenQuestion3").style.display == "block"){
+    document.getElementById("hiddenQuestion3").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion3").style.display = "block";
+  }
+
+}
+function hideQuestion4() {
+  if(document.getElementById("hiddenQuestion4").style.display == "block"){
+    document.getElementById("hiddenQuestion4").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion4").style.display = "block";
+  }
+
+}
+function hideQuestion5() {
+  if(document.getElementById("hiddenQuestion5").style.display == "block"){
+    document.getElementById("hiddenQuestion5").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion5").style.display = "block";
+  }
+
+}
+function hideQuestion6() {
+  if(document.getElementById("hiddenQuestion6").style.display == "block"){
+    document.getElementById("hiddenQuestion6").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion6").style.display = "block";
+  }
+
+}
+function hideQuestion7() {
+  if(document.getElementById("hiddenQuestion7").style.display == "block"){
+    document.getElementById("hiddenQuestion7").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion7").style.display = "block";
+  }
+
+}
+function hideQuestion8() {
+  if(document.getElementById("hiddenQuestion8").style.display == "block"){
+    document.getElementById("hiddenQuestion8").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion8").style.display = "block";
+  }
+
+}
+function hideQuestion9() {
+  if(document.getElementById("hiddenQuestion9").style.display == "block"){
+    document.getElementById("hiddenQuestion9").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion9").style.display = "block";
+  }
+
+}
+function hideQuestion10() {
+  if(document.getElementById("hiddenQuestion10").style.display == "block"){
+    document.getElementById("hiddenQuestion10").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion10").style.display = "block";
+  }
+
+}
+function hideQuestion11() {
+  if(document.getElementById("hiddenQuestion11").style.display == "block"){
+    document.getElementById("hiddenQuestion11").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion11").style.display = "block";
+  }
+
+}
+function hideQuestion12() {
+  if(document.getElementById("hiddenQuestion12").style.display == "block"){
+    document.getElementById("hiddenQuestion12").style.display = "none";
+  }else{
+    document.getElementById("hiddenQuestion12").style.display = "block";
+  }
+
+}
+
+document.addEventListener('mousedown', function (event) {
+  if (event.detail > 1) {
+    event.preventDefault(); // of course, you still do not know what you prevent here... You could also check event.ctrlKey/event.shiftKey/event.altKey to not prevent something useful.
+  }
+}, false);
